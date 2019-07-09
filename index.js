8a6e09747d269e913e3b4dd3c2fdac5a7637d684
Lamberto <asghedom.lamberto@gmail.com>
2019-07-09 06:59:07 -0700

    GET /api/users/:id

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ server.get("/api/users", (req, res) => {
             res.status(200).json(db)
         })
         .catch(err => {
+            res.status(500).json({ errorMessage: "The users information could not be retrived" })
+        })
+})
+
+// GET users with a specific id
+server.get("/api/users/:id", (req, res) => {
+    const { id } = req.params;
+    db.findById(id)
+        .then(id => {
+            res.status(201).json(id)
+        })
+        .catch(err => {
             res.status(500).json(err)
         })
 })
