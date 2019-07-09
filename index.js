dedadd0a742808b292e2724f3db6c64cf4f781f1
Lamberto <asghedom.lamberto@gmail.com>
2019-07-08 22:40:58 -0700

    GET /api/users

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // implement your API here
+const express = require("express")
+
+const db = require('./data/db.js');
+const server = express();
+
+server.get("/", (req, res) => {
+    res.send("NODE CHALLENGE")
+})
+
+//  R in CRUD <<<<<<<<
+server.get("/api/users", (req, res) => {
+    db.find()
+        .then(db => {
+            res.status(200).json(db)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+const port = 8000;
+server.listen(port, () => console.log(`\n*** running on port ${port} ***\n`))
